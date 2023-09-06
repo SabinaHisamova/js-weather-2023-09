e9a95cc749ae402a4b63b60a718fda77fc27d530
DESKTOP-DKTVU9O\Sabina <hisamovasabina5@gmail.com>
2023-09-06 20:31:47 +0500

    add jest

---
+++ sum.test.js
@@ -0,0 +1,14 @@
+import { sum } from './sum';
+
+describe('sum function', () => {
+
+    it("sum is a function", () => {
+      expect(sum).toBeInstanceOf(Function);
+    });
+  
+    it("sum of (5,10)", () => {
+      expect(sum(5,10)).toBe(15);
+    });
+  
+  });
+  

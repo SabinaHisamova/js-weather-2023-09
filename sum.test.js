e927f65c0e150cb987b0391e5ea093250295e29b
DESKTOP-DKTVU9O\Sabina <hisamovasabina5@gmail.com>
2023-09-06 20:54:12 +0500

    add eslint

--- sum.test.js
+++ sum.test.js
@@ -1,14 +1,11 @@
 import { sum } from './sum';
 
 describe('sum function', () => {
-
-    it("sum is a function", () => {
-      expect(sum).toBeInstanceOf(Function);
-    });
-  
-    it("sum of (5,10)", () => {
-      expect(sum(5,10)).toBe(15);
-    });
-  
+  it('sum is a function', () => {
+    expect(sum).toBeInstanceOf(Function);
   });
-  
+
+  it('sum of (5,10)', () => {
+    expect(sum(5, 10)).toBe(15);
+  });
+});
